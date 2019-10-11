588ff104872a1aee4f345b76f87b03f7f80fece0
Hamon <hamonaxel@live.fr>
2019-10-11 09:40:01 +0200

    Ajout du fichier "main". Début du codage de l'interface et des choix.

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        menuPrincipal();
+    }
+
+    public static void menuPrincipal() {
+        System.out.println("======================================");
+        System.out.println("=========== MENU PRINCIPAL ==========");
+        System.out.println("======================================");
+        System.out.println("1 - Blablabla ");
+        }
+}
